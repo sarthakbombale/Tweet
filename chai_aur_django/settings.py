6a417097b2b13a054161e4e0af5c7d8fdc09da2a
sarthakbombale <sarthakbombale250@gmail.com>
2026-07-01 16:10:19 +0530

    Crud Operation Perfom  & UI Enhance

--- chai_aur_django/settings.py
+++ chai_aur_django/settings.py
@@ -118,10 +118,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Authentication redirect settings
+LOGIN_URL = 'login'
+LOGIN_REDIRECT_URL = 'tweet_list'
+LOGOUT_REDIRECT_URL = 'index'
--- tweet/urls.py
+++ tweet/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('tweets/', views.tweet_list, name='tweet_list'),
     path('tweets/create/', views.tweet_create, name='tweet_create'),
     path('tweets/<int:tweet_id>/edit/', views.tweet_edit, name='tweet_edit'),
